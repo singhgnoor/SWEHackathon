fda527290f684f370d0507ae84017efa67b2d77f
Gurnoor Singh <72779640+singhgnoor@users.noreply.github.com>
2025-10-05 19:38:02 +0530

    Configure Django for static files and deployment
    Added WhiteNoise middleware and static files settings to settings.py for proper static file handling in deployment. Updated .idea project files for Django support and Python 3.12 SDK. Added requirements.txt and collected static admin files for deployment.

--- SWEHackathon/settings.py
+++ SWEHackathon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-9r^0f!!l%q2i1$_y51=jv%$12zr*vl%k!wnyf-r%pobx+=wmig
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swehack.onrender.com/']
 
 
 # Application definition
@@ -47,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # For staticfiles
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'SWEHackathon.urls'
@@ -116,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
